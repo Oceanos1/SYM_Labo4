2cd3a2957003e1ace3d0a9faf654159e6870b335
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-01-27 21:40:58 +0100

    Notifications done

--- mobile/src/main/java/ch/heigvd/iict/sym/sym_labo4/NotificationActivity.java
+++ mobile/src/main/java/ch/heigvd/iict/sym/sym_labo4/NotificationActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.RemoteInput;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,6 +16,8 @@ import android.widget.Toast;
 import ch.heigvd.iict.sym.wearcommon.Constants;
 
 public class NotificationActivity extends AppCompatActivity {
+
+    public static final String EXTRA_VOICE_REPLY = "extra_voice_reply";
 
     private Button pendingButton;
     private Button actionButton;
@@ -52,11 +55,74 @@ public class NotificationActivity extends AppCompatActivity {
                 notificationManager.notify(1, notificationBuilder.build());
             }
         });
-        /* A IMPLEMENTER */
 
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int id = 1;
+                PendingIntent pendingIntent = createPendingIntent(0, "Notification has been clicked !");
+
+                NotificationCompat.Builder notificationBuilder =
+                        new NotificationCompat.Builder(NotificationActivity.this, "SYM")
+                                .setSmallIcon(R.drawable.ic_lightbulb_on_black_18dp)
+                                .setContentTitle("New notification")
+                                .setContentText("New pending notification")
+                                .setContentIntent(pendingIntent)
+                                .addAction(R.drawable.ic_lightbulb_on_grey600_18dp,
+                                        "OK", pendingIntent);
+
+
+                NotificationManagerCompat notificationManager =
+                        NotificationManagerCompat.from(NotificationActivity.this);
+
+                notificationManager.notify(1, notificationBuilder.build());
+            }
+        });
+
+        wearableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int id = 1;
+                PendingIntent phonePendingIntent = createPendingIntent(0, "The user said he's ok !");
+                PendingIntent wearPendingIntent = createPendingIntent(0);
+
+                RemoteInput remoteInput = new RemoteInput.Builder(EXTRA_VOICE_REPLY)
+                        .setLabel("Are you okay ?")
+                        .setChoices(new String[]{"Yes", "No"})
+                        .build();
+
+                // Create the wearable-specific action
+                NotificationCompat.Action action =
+                        new NotificationCompat.Action.Builder(R.drawable.ic_message_bulleted_white_18dp,
+                                "Reply", wearPendingIntent)
+                                .addRemoteInput(remoteInput)
+                                .build();
+
+
+                NotificationCompat.WearableExtender wearableExtender =
+                        new NotificationCompat.WearableExtender()
+                                .setHintHideIcon(true)
+                                .addAction(action);
+
+                NotificationCompat.Builder notificationBuilder =
+                        new NotificationCompat.Builder(NotificationActivity.this, "SYM")
+                                .setSmallIcon(R.drawable.ic_lightbulb_on_black_18dp)
+                                .setContentTitle("Are you ok ?")
+                                .setContentText("Really ok ?")
+                                .setContentIntent(phonePendingIntent)
+                                .addAction(R.drawable.ic_lightbulb_on_grey600_18dp,
+                                        "I'm ok, no choice", phonePendingIntent)
+                                .extend(wearableExtender);
+
+
+                NotificationManagerCompat notificationManager =
+                        NotificationManagerCompat.from(NotificationActivity.this);
+
+                notificationManager.notify(1, notificationBuilder.build());
+            }
+        });
     }
 
-    /* A IMPLEMENTER */
 
     /*
      *  Code fourni pour les PendingIntent
@@ -73,6 +139,41 @@ public class NotificationActivity extends AppCompatActivity {
         if (intent == null) return;
         if (Constants.MY_PENDING_INTENT_ACTION.equals(intent.getAction()))
             Toast.makeText(this, "" + intent.getStringExtra("msg"), Toast.LENGTH_SHORT).show();
+        if (Constants.MY_PENDING_INTENT_ACTION_FROM_WEAR.equals(intent.getAction()))
+            Toast.makeText(this, "The user said: " + getMessageText(intent), Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Used to extract what the user said with the wearable
+     * @param intent
+     * @return
+     */
+    private CharSequence getMessageText(Intent intent) {
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+        if (remoteInput != null) {
+            return remoteInput.getCharSequence(EXTRA_VOICE_REPLY);
+        }
+        return null;
+    }
+
+    /**
+     * Method used to create a PendingIntent without any message
+     * The intent will start a new activity Instance or bring to front an existing one.
+     * See parentActivityName and launchMode options in Manifest
+     * See https://developer.android.com/training/notify-user/navigation.html for TaskStackBuilder
+     *
+     * @param requestCode The request code
+     * @return The pending Intent
+     */
+    private PendingIntent createPendingIntent(int requestCode) {
+        Intent myIntent = new Intent(NotificationActivity.this, NotificationActivity.class);
+        myIntent.setAction(Constants.MY_PENDING_INTENT_ACTION_FROM_WEAR);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(NotificationActivity.class);
+        stackBuilder.addNextIntent(myIntent);
+
+        return stackBuilder.getPendingIntent(requestCode, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     /**
